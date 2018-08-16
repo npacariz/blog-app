9a4a0670cfe6f4d9d7a9499fffbf70d183c22703
npacariz <nikolapacariz@gmail.com>
2018-08-16 23:02:19 +0200

    Init commit

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import VueRoute from "vue-route";
+
+Vue.use(VueRoute);
+
+const routes = [
+  {
+    path: "/",
+    redirict: "/posts"
+  },
+  {
+      path: '/posts'
+      component: 
+  }
+];
